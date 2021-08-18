0c9f326bb73088d18c779be7e25b811e2c41f3c4
Roshan KC <62145750+yagamipal@users.noreply.github.com>
2021-08-18 21:29:14 +0545

    Add files via upload

---
+++ plugins/nepdate.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+from userge import userge, Message
+
+@userge.on_cmd("td", about={
+    'header': "Tell present nepali date",
+    'usage': "!td"})
+async def nepdate(message: Message):
+         nefoli = []
+         URL = "https://english.hamropatro.com/"
+         page = requests.get(URL)
+         soup = BeautifulSoup(page.content, "html.parser")
+         time_div = soup.find_all("div", class_ = "time")
+         date_div = soup.find_all("div", class_ = "date")
+
+         for date in date_div:
+             request_date = date.find(class_='nep')
+             datevalue = request_date.text.strip()
+             nefoli.append(f"🇳🇵 {datevalue}")
+             break;
+
+         event_div = soup.find(class_ = "events")
+         eventvalue1 = event_div.text.strip()
+         nefoli.append(f"📄 {eventvalue1}")
+
+   
+         for time in time_div:
+             request_time = time.find("span")
+             engdate = time.find("span", class_ = "eng")
+             timevalue = '🕒 ' + request_time.text.strip() + "\n" + '📅 ' + engdate.text.strip()
+             nefoli.append(timevalue)
+             break;
+         todaynefol = '\n'.join(nefoli)   
+         await message.edit(todaynefol)
+  
