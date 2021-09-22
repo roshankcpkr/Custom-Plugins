aa97d2d086e4f879e765a65fa1cdb70889701c0f
Roshan KC <62145750+yagamipal@users.noreply.github.com>
2021-09-22 12:19:50 +0545

    Update rashi.py

--- plugins/rashi.py
+++ plugins/rashi.py
@@ -8,16 +8,18 @@ from userge import userge, Message
 async def rashi(message: Message):
          csymbol = message.input_str
          symbol = csymbol.lower()
-         URL = f"https://www.hamropatro.com/rashifal/daily/{symbol}"
-         page = requests.get(URL)
-         soup = BeautifulSoup(page.content, "html.parser")
-         rashi_div = soup.find_all("div", class_="desc")
-
-         for rashifal in rashi_div:
-            request_rashi = rashifal.find("p")
-            rashivalue = request_rashi.text.strip()
-            await message.edit(csymbol.title() + ': '+ '\n' + rashivalue)
+         if symbol == True:
             
+                 URL = f"https://www.hamropatro.com/rashifal/daily/{symbol}"
+                 page = requests.get(URL)
+                 soup = BeautifulSoup(page.content, "html.parser")
+                 rashi_div = soup.find_all("div", class_="desc")
+                for rashifal in rashi_div:
+                    request_rashi = rashifal.find("p")
+                    rashivalue = request_rashi.text.strip()
+                    await message.edit(csymbol.title() + ': '+ '\n' + rashivalue)
+          else:
+            await message.edit("Enter correct rasifal gay")
             
 @userge.on_cmd("allrashi", about={
     'header': "Get list of rashifal name",
